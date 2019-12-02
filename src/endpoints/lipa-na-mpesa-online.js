594e9ea35355951fadadfc27bbd7ed77e43319b0
Carsten Munk <carsten.munk@zippie.org>
2019-12-02 09:58:32 +0100

    Add seperate business short code and lipa na m-pesa short code
    Signed-off-by: Carsten Munk <carsten.munk@zippie.org>

--- src/endpoints/lipa-na-mpesa-online.js
+++ src/endpoints/lipa-na-mpesa-online.js
@@ -13,14 +13,15 @@
  * @param  {string} [passKey=null]                            Lipa na mpesa passKey
  * @return {Promise}
  */
-module.exports = async function (senderMsisdn, amount, callbackUrl, accountRef, transactionDesc = 'Lipa na mpesa online', transactionType = 'CustomerPayBillOnline', shortCode = null, passKey = null) {
+module.exports = async function (senderMsisdn, amount, callbackUrl, accountRef, transactionDesc = 'Lipa na mpesa online', transactionType = 'CustomerPayBillOnline', shortCode = null, passKey = null, businessShortCode = null) {
+  const _businesShortCode = (businessShortCode || shortCode) || this.configs.lipaNaMpesaShortCode
   const _shortCode = shortCode || this.configs.lipaNaMpesaShortCode
   const _passKey = passKey || this.configs.lipaNaMpesaShortPass
   const timeStamp = (new Date()).toISOString().replace(/[^0-9]/g, '').slice(0, -3)
-  const password = Buffer.from(`${_shortCode}${_passKey}${timeStamp}`).toString('base64')
+  const password = Buffer.from(`${_businessShortCode}${_passKey}${timeStamp}`).toString('base64')
   const req = await this.request()
   return req.post('/mpesa/stkpush/v1/processrequest', {
-    'BusinessShortCode': _shortCode,
+    'BusinessShortCode': _businesShortCode,
     'Password': password,
     'Timestamp': timeStamp,
     'TransactionType': transactionType,
